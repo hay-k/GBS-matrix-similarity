b2c44c1e6866173051f652c64428906b15681013
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2019-10-24 22:04:39 +0400

    initalize the project

---
+++ core/utils.py
@@ -0,0 +1,100 @@
+'''
+
+'''
+import numpy as np
+from openfermion.hamiltonians import MolecularData
+from openfermionpyscf import run_pyscf
+from openfermion.utils import geometry_from_pubchem
+import gbsapps
+import gbsapps.sample
+
+def round_custom(num, thresh=0.0001):
+    if num < thresh:
+        num = round(num)
+        if num == -0:
+            return 0
+    return num    
+
+def get_adjacency_matrix(single_body, two_body):
+    x,y = single_body.shape
+    func =  np.vectorize(round_custom)
+    _new_dim = x * y
+    single_one_dim = single_body.reshape(_new_dim, 1)
+    two_body_two_dim = func(two_body.reshape(_new_dim, _new_dim))
+    idx = 0
+    x, _ = two_body_two_dim.shape
+    while idx < x:
+        two_body_two_dim[idx][idx] = round_custom(single_one_dim[idx][0])
+        idx += 1
+    return two_body_two_dim
+
+def get_single_two_body_h2():
+    geometry = [['H', [0, 0, 0]], ['H', [0, 0, 0.74]]] # H--H distance = 0.74pm
+    basis = 'sto-3g'
+    multiplicity = 1 #(2S+1)
+    charge = 0
+    h2_molecule = MolecularData(geometry, basis, multiplicity, charge)
+    h2_molecule = run_pyscf(h2_molecule)
+    return h2_molecule.one_body_integrals, h2_molecule.two_body_integrals
+
+def get_single_two_body(molecule_name, basis='sto-3g', multiplicity=1):
+    geometry = geometry_from_pubchem(molecule_name)
+    _molecule = MolecularData(geometry, basis, multiplicity)
+    _molecule = run_pyscf(_molecule)
+    # save the molecule after running pyscf.
+    return _molecule.one_body_integrals, _molecule.two_body_integrals
+
+def get_single_two_body_file(molecule_file_name):
+    _molecule = MolecularData(filename=molecule_file_name)
+    _molecule.load()
+#    _molecule = run_pyscf(_molecule)
+    return _molecule.one_body_integrals, _molecule.two_body_integrals
+
+def wrapper(mol1, mol2):
+    a1, b1 = get_single_two_body(mol1)
+    adj1 = get_adjacency_matrix(a1, b1)
+    a2, b2 = get_single_two_body(mol2)
+    adj2 = get_adjacency_matrix(a2, b2)
+    return calculate_similarity(adj1, adj2)
+    
+def calculate_similarity(mol1_adj, mol2_adj):
+    diff = mol1_adj - mol2_adj
+    diff_dot = np.dot(diff, np.transpose(np.conjugate(diff)))
+    if np.linalg.norm(diff_dot) == 0:
+        return 1-np.sqrt(np.trace(diff_dot))
+    else:
+        return 1-np.sqrt(np.trace(diff_dot)/(np.trace(diff)**2))
+
+def process_fidelity(U1, U2, normalize=True):
+    """
+    Calculate the process fidelity given two process operators.
+    """
+    if normalize:
+        return np.trace(np.dot(U1, U2)) / (np.trace(U1) * np.trace(U2))
+    else:
+        return np.trace(np.dot(U1, U2))    
+    
+def run_gbs(adjacency_mat, number_of_times, num_val=4, threshold=False):
+    idx = 0
+    while idx < number_of_times:
+        print(gbsapps.sample.quantum_sampler(adjacency_mat, num_val, threshold))
+        idx += 1
+    
+def get_state_map(num_bits):
+    if num_bits == 3:
+        return {
+            '11': ['110', '011', '101'],
+            '2': ['002', '020', '200'],
+            '22': ['022', '202', '220'],
+            '13': ['013', '103', '130', '031', '301', '310'],
+            '121': ['121', '211', '112'],
+            '4': ['004', '040', '004'],
+            '123': ['123', '213', '231', '132', '312', '321']
+        }
+    elif num_bits == 5:
+        return {
+            '11': ['00011', '00110', '01100', '11000', '00101', '01010', '10100', '01001', '10010', '10001'],
+            '13': ['00031', '00013', '00301', '00103', '03010', '01030', '30100', '10300', '30010', '10030', '30001', '10003'],
+            '22': ['00022', '00202', '00220', '02020', '02200', '20200', '20020', '02002', '20002'],
+            '112': ['00211', '00121', '00112', '02110', '01210', '01120', '21100', '12100', '11200', '02011', '01021'],
+        }
