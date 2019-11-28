709cc1c3f4d3577469b430b15573aca3ac8b4dc6
hay-k <52532457+hay-k@users.noreply.github.com>
2019-11-28 11:39:09 +0400

    polish the code in utils

--- core/main.py
+++ core/main.py
@@ -1,18 +1,24 @@
 import numpy as np
-from itertools import permutations
-
+import core.molecular_data as md
 from core.gbs import GBSDevice
+from openfermion.utils import geometry_from_pubchem
+import pubchempy
 
 
 if __name__ == '__main__':
-    matrix = np.array([[1, 1, 1],
-                       [1, 1, 1],
-                       [1, 1, 1]])
+    # matrix = np.array([[1, 1, 1],
+    #                    [1, 1, 1],
+    #                    [1, 1, 1]])
+    #
+    # molecule = GBSDevice(name='test_molecule')
+    # molecule.encode_matrix(matrix, 5)
+    #
+    # print(molecule.get_feature_vector(3))
 
-    molecule = GBSDevice(name='test_molecule')
-    molecule.encode_matrix(matrix, 5)
-
-    print(molecule.get_feature_vector(3))
-
-
-
+    # print(md.get_single_two_body("H2"))
+    # geometry = geometry_from_pubchem("O2")
+    # print(geometry)
+    name = 'O2'
+    pubchempy_2d_molecule = pubchempy.get_compounds(name, 'name')
+    print(pubchempy_2d_molecule[0].to_dict())
+    print(geometry_from_pubchem(name))
