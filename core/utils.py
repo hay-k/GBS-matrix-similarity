1828406adfb8955105af9dd821354ae4cfcfd06e
Anupam Mediratta <mediratta@gmail.com>
2019-11-29 20:01:02 +0000

    adding call to gbsapps

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+mols = ["H2", "LiH", "O2", "N2", "F2", "Ne2", "Ar2", "CO", "HCN", "HNC", "CH4", "H2O", "NH3", "BH3", "H2O2", "H2CO", "HCOOH", "CH3OH", "CH3CH2OH"]
+
+large_mols = ["Ne2"]
+
+not_present = ["Ar2", "CO"]
 
 def wrapper_get_single_two_body():
   for item in mols:
