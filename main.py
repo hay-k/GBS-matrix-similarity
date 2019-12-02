4aac695844cafcf3996e1a3044d2606377c3683f
Anupam Mediratta <mediratta@gmail.com>
2019-12-02 13:47:01 +0000

    checking some test code

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from itertools import permutations
+
+from core.gbs import GBSDevice
+from core.molecular_data import get_single_two_body, get_adjacency_matrix
+
+
+if __name__ == '__main__':
+    matrix = np.array([[1, 1, 1],
+                       [1, 1, 1],
+                       [1, 1, 1]])
+
+    molecule = GBSDevice(name='test_molecule')
+    molecule.encode_matrix(matrix, 5)
+
+    print(molecule.get_feature_vector(3))
+    
+    
+def test_molecule(mol, n_mean=5, max_photons=5):
+    a,b = get_single_two_body(mol)
+    adj_matrix = get_adjacency_matrix(a,b)
+    molecule = GBSDevice(name=mol)
+    print(molecule.encode_matrix(matrix, n_mean))
+    print(molecule.get_feature_vector(max_photons))
