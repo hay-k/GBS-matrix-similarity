0b0fe503388b684ef4d4bab77827e1ec2b50aad8
hay-k <52532457+hay-k@users.noreply.github.com>
2019-12-11 19:07:31 +0400

    add another small test for gbs

--- tests/test_gbs.py
+++ tests/test_gbs.py
@@ -1,11 +1,31 @@
 from functools import partial
 from numpy import ones, mean, std
+from math import factorial
+from itertools import permutations
 from core.gbs import GBSDevice
 import unittest
 import timeit
 
 
 class GBSDeviceTimingTest(unittest.TestCase):
+
+    def test_feature_vector(self):
+        mode_count = 10
+        max_photons = 5
+        molecule = GBSDevice(name='test_molecule', mode_count=mode_count)
+        orbit_reps = molecule.get_all_orbit_representatives(max_photons)
+
+        all_patterns = []
+        for orbit in orbit_reps:
+            all_patterns.extend(set(permutations(orbit)))
+        expected_length = 0
+
+        for i in range(max_photons + 1):
+            # count the number of all possible detection events with total photon number up to (including) max_photons
+            _tmp_len = factorial(mode_count + i - 1) // (factorial(i) * factorial(mode_count - 1))
+            expected_length += _tmp_len
+
+        self.assertEqual(len(all_patterns), expected_length)
 
     def test_three_photons(self):
         times = []
