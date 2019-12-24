6b99286c494357e814d994a61c9f23429125acef
hay-k <52532457+hay-k@users.noreply.github.com>
2019-12-24 22:02:33 +0400

    some example usage of the new functions

--- core/main.py
+++ core/main.py
@@ -1,12 +1,18 @@
 import numpy as np
 from core.gbs import GBSDevice
+import strawberryfields as sf
+import networkx as nx
 
 if __name__ == '__main__':
-    matrix = np.array([[1, 1, 1],
-                       [1, 1, 1],
-                       [1, 1, 1]])
+    # generate a random symmetric matrix
+    matrix = np.random.rand(10, 10)
+    matrix = matrix + matrix.T
 
     molecule = GBSDevice(name='test_molecule')
     molecule.encode_matrix(matrix, 5)
 
-    print(molecule.get_feature_vector(3))
+    print("Exact feature vector of events:", molecule.get_event_feature_vector(2, max_photons_per_mode=2))
+    print("MC feature vector of events:", molecule.get_event_feature_vector(2, max_photons_per_mode=2, mc=True))
+
+    print("Exact feature vector of orbits:", molecule.get_orbit_feature_vector(2))
+    print("MC feature vector of orbits:", molecule.get_orbit_feature_vector(2, mc=True))
